8ea09ee20ba22a150a5f387d49237eb0e650eba5
alex <aabohm7@gmail.com>
2017-12-28 00:59:42 -0600

    split into function

--- main.cpp
+++ main.cpp
@@ -23,143 +23,174 @@ void upSample(int factor = 3){
 		}
 	}
 };
+void enhaceSquare(int w, int h, int channel, int sSize = 4){
+	//temp threshold
+	int th;
+	//maximum thresholds
+	int th1 = 0;
+	int th2 = -1;
+	//maximum threshold positions
+	int th1x = 0, th1y = 0;
+	int th2x = 0, th2y = 0;
+	//find max threshold in outer ring of pixels
+	//top right two
+	for(int i = w + 1; i <  w + 3; i++){
+		th = std::abs((int)orig(i, h, channel) - (int)orig(i - 1, h, channel));
+		if(th > th1){
+			th1 = th;
+			th1x = i;
+			th1y = h;
+		}else if(th > th2 && th < th1){
+			th2 = th;
+			th2x = i;
+			th2y = h;
+		}
+	}
+	//bottom right two
+	for(int i = h + 1; i < h + 3; i++){
+		th = std::abs((int)orig(w + 2, i, channel) - (int)orig(w + 2, i - 1, channel));
+		if(th > th1){
+			th1 = th;
+			th1x = w + 2;
+			th1y = i;
+		}else if(th > th2 && th < th1){
+			th2 = th;
+			th2x = w + w;
+			th2y = i;
+		}
+	}
+	//bottom left two
+	for(int i = w; i < w + 2; i++){
+		th = std::abs((int)orig(i, h + 2, channel) - (int)orig(i + 1, h + 2, channel));
+		if(th > th1){
+			th1 = th;
+			th1x = i;
+			th1y = h + 2;
+		}else if(th > th2 && th < th1){
+			th2 = th;
+			th2x = i;
+			th2y = h + 2;
+		}
+	}
+	//top left two
+	for(int i = h; i < h + 2; i++){
+		th = std::abs((int)orig(w, i, channel) - (int)orig(w, i + 1, channel));
+		if(th > th1){
+			th1 = th;
+			th1x = w;
+			th1y = i;
+		}else if(th > th2 && th < th1){
+			th2 = th;
+			th2x = w;
+			th2y = i;
+		}
+	}
+	//done finding max thresholds
+
+	//if the square is mostly solid color
+	//just fill in the higher res image with solid color
+	if(th1 < 2 || th2 < 2){ //TODO: replace 2 with changeable thres
+		//std::cout << "upsample" << std::endl;
+		unsigned char c;
+		for(int y = h; y < h + 3; y++){
+			for(int x = w; x < w + 3; x++){
+				c = orig(x, y, channel);
+				for(int uh = 0; uh < 3; uh++){
+					for(int uw = 0; uw < 3; uw++){
+						mod( (int)(x * ((double)sSize / 3.0)) + uw, (int)(y * ((double)sSize / 3.0)) + uh, channel) = c;
+					}
+				}
+			}
+		}
+		return;
+	}
+	//apply new pixels to high res img
+	//TODO: find why this doesn't throw any errors about div by 0
+	//find slope between two colors
+	if(th1x == th2x){
+		//TODO: what if it is only two pxls
+		std::cout << "Equal" << std::endl;
+		//color1 and color2 are the two colors on either side of the line
+		//color above line
+		int color1total = 0;
+		int color1count = 1;
+		//color below line
+		int color2total = 0;
+		int color2count = 1;
+		for(int i = 0; i < 3; i++){
+			if(i <= th1x - w){
+				for(int j = 0; j < 3; j++){
+					color1total += orig(w + i, h + j, channel);
+					color1count++;
+				}
+			} else if(i > th1x - w){
+				for(int j = 0; j < 3; j++){
+					color2total += orig(w + i, h + j, channel);
+					color2count++;
+				}
+			}
+			
+		}
+		//std::cout << "Here" << std::endl;
+		unsigned char color1 = color1total / color1count;
+		unsigned char color2 = color2total / color2count;
+		//fill in areas with color1 and color2
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j < 3; j++){
+				if(i <= th1x - w){
+					mod((int)(w * ((double)sSize / 3.0)) + i, (int)(h * ((double)sSize / 3.0)) + j, channel) = color1;
+				} else if(i > th1x - w){
+					mod((int)(w * ((double)sSize / 3.0)) + i, (int)(h * ((double)sSize / 3.0)) + j, channel) = color2;
+				}
+			}
+		}
+	} else {
+		double slope = ((double)th1y - th2y) / ((double)th1x - th2x);
+		//TODO: fix undefined slope errors
+		//color1 and color2 are the two colors on either side of the line
+		//color above line
+		int color1total = 0;
+		int color1count = 1;
+		//color below line
+		int color2total = 0;
+		int color2count = 1;
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j < 3; j++){
+				if(j > i * slope){
+					color1total += orig(w + i, h + j, channel);
+					color1count++;
+				} else if(j < i * slope){
+					color2total += orig(w + i, h + j, channel);
+					color2count++;
+				}
+			}
+		}
+		//std::cout << "Here" << std::endl;
+		unsigned char color1 = color1total / color1count;
+		unsigned char color2 = color2total / color2count;
+		//std::cout << "Not Here" << std::endl;
+		//fill in square
+		//color1 is used as the color on the line
+		for(int i = 0; i < sSize; i++){
+			for(int j = 0; j < sSize; j++){
+				if(j >= i * slope){
+					mod((int)(w * ((double)sSize / 3.0)) + i, (int)(h * ((double)sSize / 3.0)) + j, channel) = color1;
+				} else if(j < i * slope){
+					mod((int)(w * ((double)sSize / 3.0)) + i, (int)(h * ((double)sSize / 3.0)) + j, channel) = color2;
+				}
+			}
+		}
+	}
+};
 //function that enhances image and puts it into a higher res image
 //TODO: once it is working make it threadsafe
-void enhanceImage(short differenceThreshold = 1){
-	unsigned char data[8];
+//(thresh, size of each square in new image)
+void enhanceImage(){
 	for(int channel = 0; channel < orig.spectrum(); channel++){
 		std::cout << "Channel " << channel << std::endl;
 		for(int h = 0; h < orig.height() - 2; h += 3){
 			for(int w = 0; w < orig.width() - 2; w += 3){
-				//temp threshold
-				int th;
-				//maximum thresholds
-				int th1 = 0;
-				int th2 = -1;
-				//maximum threshold positions
-				int th1x = 0, th1y = 0;
-				int th2x = 0, th2y = 0;
-				//find max threshold in outer ring of pixels
-				//top right two
-				for(int i = w + 1; i <  w + 3; i++){
-					th = std::abs((int)orig(i, h, channel) - (int)orig(i - 1, h, channel));
-					if(th > th1){
-						th1 = th;
-						th1x = i;
-						th1y = h;
-					}else if(th > th2 && th < th1){
-						th2 = th;
-						th2x = i;
-						th2y = h;
-					}
-				}
-				//bottom right two
-				for(int i = h + 1; i < h + 3; i++){
-					th = std::abs((int)orig(w + 2, i, channel) - (int)orig(w + 2, i - 1, channel));
-					if(th > th1){
-						th1 = th;
-						th1x = w + 2;
-						th1y = i;
-					}else if(th > th2 && th < th1){
-						th2 = th;
-						th2x = w + w;
-						th2y = i;
-					}
-				}
-				//bottom left two
-				for(int i = w; i < w + 2; i++){
-					th = std::abs((int)orig(i, h + 2, channel) - (int)orig(i + 1, h + 2, channel));
-					if(th > th1){
-						th1 = th;
-						th1x = i;
-						th1y = h + 2;
-					}else if(th > th2 && th < th1){
-						th2 = th;
-						th2x = i;
-						th2y = h + 2;
-					}
-				}
-				//top left two
-				for(int i = h; i < h + 2; i++){
-					th = std::abs((int)orig(w, i, channel) - (int)orig(w, i + 1, channel));
-					if(th > th1){
-						th1 = th;
-						th1x = w;
-						th1y = i;
-					}else if(th > th2 && th < th1){
-						th2 = th;
-						th2x = w;
-						th2y = i;
-					}
-				}
-				//done finding max thresholds
-
-				//if the square is mostly solid color
-				//just fill in the higher res image with solid color
-				if(th1 < differenceThreshold || th2 < differenceThreshold){
-					//std::cout << "upsample" << std::endl;
-					unsigned char c;
-					for(int y = h; y < h + 3; y++){
-						for(int x = w; x < w + 3; x++){
-							c = orig(x, y, channel);
-							for(int uh = 0; uh < 3; uh++){
-								for(int uw = 0; uw < 3; uw++){
-									mod( x*3 + uw, y*3 + uh, channel) = c;
-								}
-							}
-						}
-					}
-					continue;
-				}
-				//apply new pixels to high res img
-				//TODO: find why this doesn't throw any errors about div by 0
-				//find slope between two colors
-				double slope = ((double)th1y - th2y) / ((double)th1x - th2x);
-
-				/* draw line between max thresholds
-				for(int i = 0; i < 9; i++){
-					//don't draw the pixel if the line goes past the borders
-					if(i * slope > 8 || i * slope < 0){
-						break;
-					}
-					//draw a pixel for the dividing line
-					mod(w * 3 + i, h * 3 + i * slope, channel) = 255;
-				}
-				//*/
-				//TODO: fix undefined slope errors
-				//color1 and color2 are the two colors on either side of the line
-				//color above line
-				int color1total = 0;
-				int color1count = 1;
-				//color below line
-				int color2total = 0;
-				int color2count = 1;
-				for(int i = 0; i < 3; i++){
-					for(int j = 0; j < 3; j++){
-						if(j > i * slope){
-							color1total += orig(w + i, h + j, channel);
-							color1count++;
-						} else if(j < i * slope){
-							color2total += orig(w + i, h + j, channel);
-							color2count++;
-						}
-					}
-				}
-				//std::cout << "Here" << std::endl;
-				unsigned char color1 = color1total / color1count;
-				unsigned char color2 = color2total / color2count;
-				//std::cout << "Not Here" << std::endl;
-				//fill in square
-				//color1 is used as the color on the line
-				for(int i = 0; i < 9; i++){
-					for(int j = 0; j < 9; j++){
-						if(j >= i * slope){
-							mod(w * 3 + i, h * 3 + j, channel) = color1;
-						} else if(j < i * slope){
-							mod(w * 3 + i, h * 3 + j, channel) = color2;
-						}
-					}
-				}
+				enhaceSquare(w, h, channel, 6);
 			}
 		}
 	}
@@ -173,9 +204,9 @@ int main(int argc, char const *argv[])
 		orig = CImg<unsigned char>(argv[1]);
 	}
 	//setup image to write enhanced original to (for now 3x higher res)
-	mod = CImg<unsigned char>(orig.width() * 3, orig.height() * 3, orig.depth(), orig.spectrum(), 0);
+	mod = CImg<unsigned char>(orig.width() * 6.0 / 3.0 + 1, orig.height() * 6.0 / 3.0 + 1, orig.depth(), orig.spectrum(), 0);
 	//pass enhancement function a threshold of 2
-	enhanceImage(2);
+	enhanceImage();
 	//save image
 	std::cout << "Saving Image" << std::endl;
 	//TODO: consider using other format to save image (lossless?)
